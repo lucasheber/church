773d8065cf9de70a6546d063c2e80498fe3bfddc
Lucas Heber <lucas.heber07@gmail.com>
2025-06-10 13:59:32 -0300

    feat(config): add Ziggy configuration and integrate with Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { ZiggyVue } from 'ziggy-js';
 
 createInertiaApp({
   resolve: name => {
@@ -10,6 +11,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
   },
 })
