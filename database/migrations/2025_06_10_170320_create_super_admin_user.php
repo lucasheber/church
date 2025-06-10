8e247605a7b536bd9619254d0980381dad810827
Lucas Heber <lucas.heber07@gmail.com>
2025-06-10 14:10:29 -0300

    feat(migrations): create migration for Super Admin user and role

---
+++ database/migrations/2025_06_10_170320_create_super_admin_user.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $role = Role::firstOrCreate(['name' => 'Super Admin']);
+
+        $user = User::firstOrCreate(
+            [
+                'name' => 'Super Admin',
+                'email' => 'superadmin@example.com',
+                'password' => bcrypt('password'),
+            ]
+        );
+
+        $role->users()->attach($user);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $role = Role::where('name', 'Super Admin')->first();
+
+        if ($role) {
+            $user = DB::table('users')->where('email', 'superadmin@example.com')->first();
+            if ($user) {
+                $role->users()->detach($user);
+                DB::table('users')->where('email', 'superadmin@example.com')->delete();
+            }
+        }
+    }
+};
