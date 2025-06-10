23f5c8d9f54c9553b30f391eb2731e1b7df88933
Lucas Heber <lucas.heber07@gmail.com>
2025-06-10 16:12:02 -0300

    feat(view): update app.blade.php to include CSS in vite directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
+
     @routes
-    @vite('resources/js/app.js')
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
 
